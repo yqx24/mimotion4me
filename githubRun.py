0472cb67f64812da4fecc6a54ba84a36b0a47192
yqx24 <540528409@qq.com>
2024-08-25 12:50:20 +0800

    Update githubRun.py

--- githubRun.py
+++ githubRun.py
@@ -195,6 +195,6 @@ if __name__ == '__main__':
     password = sys.argv[2]
     pushplus_token = sys.argv[3]
     ##刷步数
-    res = main(user, password, 5000, 8000)
+    res = main(5405248409@qq.com, Yy123456, 5000, 8000)
     # 推送消息
     pushMessage(pushplus_token, "刷步接口调用", res)
